7d590f2a3bafb308a46e61bba61555a301d3d78f
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-08 08:30:12 -0400

    Make question and answer fields manditory

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -5,4 +5,12 @@ router.get('/new', (req, res) => {
   res.render('new-quiz');
 });
 
+router.post('/new', (req, res) => {
+  const { question, answer } = req.body;
+  console.log(req.body);
+  // INSERT data objects into DB HERE
+  res.render('new-quiz');
+});
+
+
 module.exports = router;
