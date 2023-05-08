56dd39dc35589da9e7a3f5cc4fbe4b4d568a7652
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-08 09:03:15 -0400

    Require queries/quizzes.js in routes/quizzes.js file

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,3 +1,5 @@
+const submitQuiz = require('../db/queries/quizzes');
+
 const express = require('express');
 const router  = express.Router();
 
@@ -7,9 +9,9 @@ router.get('/new', (req, res) => {
 
 router.post('/new', (req, res) => {
   const { question, answer } = req.body;
-  console.log(req.body);
-  // INSERT data objects into DB HERE
-  res.render('new-quiz');
+  //console.log(req.body);  //Confirmed working
+  // INSERT data objects into DB HERE once queries are written
+  res.render('new-quiz');  //Render new quiz page again so user can submit another question
 });
 
 
