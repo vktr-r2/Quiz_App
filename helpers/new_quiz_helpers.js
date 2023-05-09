1be6e7f22732abe850094d4f50a6890919b03480
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-09 17:13:33 -0400

    Install cookie-parser package

--- helpers/new_quiz_helpers.js
+++ helpers/new_quiz_helpers.js
@@ -1,3 +1,5 @@
+//OBTAIN PRIVATE ID
+//Generate random 10 character string to use for private_id
 const generateRandomString = () => {
   let randomString = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -8,7 +10,7 @@ const generateRandomString = () => {
   return randomString;
 };
 
-
+//Check quizObject submitted from form.  If quizObject.private is truthy then generate random string and add as private_id to existing quiz object
 const addPrivateId = (quizObject) => {
   if (quizObject.private) {
     let privateId = generateRandomString();
@@ -18,6 +20,10 @@ const addPrivateId = (quizObject) => {
   return quizObject;
 };
 
+//OBTAIN USER ID
+const checkUserId = () => {
+
+};
 
 
 module.exports = {
