bbf353727d8232aa88c2cb2362fd8a6d5d1d3010
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-09 12:23:26 -0400

    updating db

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -8,10 +8,3 @@ INSERT INTO questions (quiz_id, question_number, question) VALUES (4, 2, 'What i
 
 INSERT INTO questions (quiz_id, question_number, question) VALUES (5, 1, 'How do you measure the volume of liquid?');
 
-
-
--- INSERT INTO questions (quiz_id, question_number, question) VALUES (1, 1, 'What is your name?');
--- INSERT INTO questions (quiz_id, question_number, question) VALUES (2, 1, 'How many states are in the United States of America');
--- INSERT INTO questions (quiz_id, question_number, question) VALUES (3, 1, 'Who sang Halo?');
--- INSERT INTO questions (quiz_id, question_number, question) VALUES (4, 1, 'How many airports are in Toronto?');
--- INSERT INTO questions (quiz_id, question_number, question) VALUES (5, 1, 'What a baby cow called?');
