a93cf9a00c5f009376b83ff58a7e4ea7ecdeb802
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-09 12:22:43 -0400

    creating helpers folder

--- db/seeds/04_answers.sql
+++ db/seeds/04_answers.sql
@@ -32,30 +32,3 @@ INSERT INTO answers (question_id, quiz_id, answer) VALUES (5, 5, 'kilometers');
 
 
 
-
-
-
--- INSERT INTO answers (question_id, quiz_id, answer, is_correct) VALUES (1, 1, 'Motunde', TRUE);
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (1, 1, 'Chansoo');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (1, 1, 'Victor');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (1, 1, 'John');
-
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (3, 2, 'fifty two');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (3, 2, 'thirty');
--- INSERT INTO answers (question_id, quiz_id, answer, is_correct) VALUES (3, 2, 'fifty', TRUE);
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (3, 2, 'seventy');
-
--- INSERT INTO answers (question_id, quiz_id, answer, is_correct) VALUES (5, 3, 'beyonce', TRUE);
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (5, 3, 'Fergie');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (5, 3, 'Ciara');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (5, 3, 'pharell');
-
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (7, 4, '1');
--- INSERT INTO answers (question_id, quiz_id, answer, is_correct ) VALUES (7, 4, '2', TRUE);
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (7, 4, '3');
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (7, 4, '4');
-
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (9, 5, 'chick');
--- INSERT INTO answers (question_id, quiz_id, answer ) VALUES (9, 5, 'goat');
--- INSERT INTO answers (question_id, quiz_id, answer, is_correct) VALUES (9, 5, 'calf', TRUE);
--- INSERT INTO answers (question_id, quiz_id, answer) VALUES (9, 5, 'kitten');
