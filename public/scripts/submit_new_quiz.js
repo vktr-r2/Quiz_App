7b38b6edd6e6e8643291ed1ae70ea4390c5f896d
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-09 02:11:31 -0400

    Update quizzes/new approach: quiz portion of form on EJS file, JQuery to append remainder of form

--- public/scripts/submit_new_quiz.js
+++ public/scripts/submit_new_quiz.js
@@ -1,46 +1,11 @@
-$(document).ready(function() {
-  let clickCount = 0;
+$(document).ready(function () {
+  let questionCount = 0;
 
-  $('#quizSubmitForm').on("submit",function(event) {
-    event.preventDefault();
-    console.log("Test first statement")
-    clickCount++;
-    if (clickCount === 1) {
-      $.ajax({
-        type: "POST",
-        url: "/quizzes/new",
-        data: $('#quizSubmitForm').serialize(),
-        success: function() {
-          $('#quizSubmitForm').hide();
-          $('#submit_question_1').show();
-        },
-        error: function() {
-          console.log("Error submitting quiz");
-        }
-      });
+  $("#next").on("click", function () {
+    if ($("#quiz-name").val()) {
+      $("#quiz-info").hide();
+      addQuestion();
     }
   });
 
-
-  $('#question_submit_button').on("click", function(event) {
-    event.preventDefault();
-    console.log("Test SECOND listener")
-    clickCount += 1;
-    console.log("Current clickCount =", clickCount);
-    if (clickCount === 3) {
-      $('#submit_question_1').hide();
-      $('#submit_question_2').show();
-    } else if (clickCount === 4) {
-      $('#submit_question_2').hide();
-      $('#submit_question_3').show();
-    } else if (clickCount === 5) {
-      $('#submit_question_3').hide();
-      $('#submit_question_4').show();
-    } else if (clickCount === 6) {
-      $('#submit_question_4').hide();
-      $('#submit_question_5').show();
-    } else if (clickCount === 7) {
-      $('#questionSubmitForm').submit();
-    }
-  });
 });
