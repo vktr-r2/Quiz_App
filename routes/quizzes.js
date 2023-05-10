2f57b858509a47859743c447bd3b8c59fce0d5b0
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-10 17:13:52 -0400

    Implement submitAnswer promise query function > quiz/questions/answers all inserting into DB successfully

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,4 +1,8 @@
-const { submitQuiz, submitQuestion, submitAnswer } = require("../db/queries/quizzes");
+const {
+  submitQuiz,
+  submitQuestion,
+  submitAnswer,
+} = require("../db/queries/quizzes");
 const questionQueries = require("../db/queries/questions");
 const answerQueries = require("../db/queries/answers");
 const { addPrivateId, addUserId } = require("../helpers/new_quiz_helpers");
@@ -13,49 +17,51 @@ router.get("/new", (req, res) => {
 ////////////////////////////////////////////
 //POST to '/new' endpoint where quiz will be submitted
 router.post("/new", (req, res) => {
-  //Adds private_id property to the submitted quiz object
+  //Adds private_id + userId properties to the submitted quiz object + store req.body in quizObj
   addPrivateId(req.body);
-
-  //Adds user_id property to the submitted quiz object
   addUserId(req);
-
-  //req.body assigned to quizOnj var
   const quizObj = req.body;
 
-  //TEST log quizObj
-  console.log("QUIZ OBJECT:", quizObj);
-
   //submitQuiz query promise function inserts row into quizzes table in db, and returns quiz_id as resolve
-  submitQuiz(
-    quizObj.user_id,
-    quizObj.quiz_name,
-    quizObj.private,
-    quizObj.private_id
-  ).then((quiz_id) => {
-    //TEST log quiz_id
-    console.log("QUIZ ID", quiz_id);
+  submitQuiz(quizObj)
+    .then((quiz_id) => {
 
-    //loop 5 times, once for each question
-    for (let i = 1; i <= 5; i++) {
-      //Use template literals to access key related to each question in quizObj
-      let questionKey = `question${i}`;
-      //questionText is the value of the questionKey property in quizObj
-      let questionText = quizObj[questionKey];
+      //OUTER loop 5 times, once for each question
+      for (let i = 1; i <= 5; i++) {
+        //Use template literals to access key related to each question in quizObj
+        let questionKey = `question${i}`;
+        //questionText is the value of the questionKey property in quizObj
+        let questionText = quizObj[questionKey];
 
-      // submitQuestion query promise function inserts row for each question into questions table.  Will loop 5 times (once for each question)
-      submitQuestion(quiz_id, i, questionText)
-        .then((question_id) => {
-          // TEST log question_id for each question
-          console.log(`QUESTION ID for ${questionKey}`, question_id);
-        })
-
-        //Handle errors from promise chain
-        .catch((err) => {
-          console.error("Error: ", err);
+        // submitQuestion query promise function inserts row for each question into questions table.  Will loop 5 times (once for each question)
+        submitQuestion(quiz_id, i, questionText)
+          .then(({ question_id, quiz_id }) => {
+          //INNER loop loops through answers submitted for each answer
+          for (let j = 1; j <= 4; j++) {
+            //Use incrementation of i and j vars to find proper key in quizObj for each answer
+            let answer = `answer${i}_${j}`;
+            //Use incrementation of i to find proper correct answer
+            let correctAnswer = `correctAnswer${i}`;
+            //Text submitted for answer stored in variable
+            let answerText = quizObj[answer];
+            //Compare correctAnswer value from quizObj to the current value of j through this loop iteration.  If true, set isCorrect to true, else set isCorrect to false
+            if (quizObj[correctAnswer] === j.toString()) {
+              isCorrect = true;
+            } else {
+              isCorrect = false;
+              }
+            // Submit the answer to the answers table and end promise chain
+            submitAnswer(question_id, answerText, isCorrect, quiz_id)
+          }
         });
-    }
-  });
+      }
+    })
+    //Handle errors from promise chain
+    .catch((err) => {
+      console.error("Error: ", err);
+    });
 
+  console.log('Quiz submitted successfully!!')
   res.render("new-quiz");
 });
 
