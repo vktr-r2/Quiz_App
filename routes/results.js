88ead1439b16efd8d1b4c08c45dd5c44af4d9400
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-10 10:52:47 -0400

    getting font style to work in body

--- routes/results.js
+++ routes/results.js
@@ -5,24 +5,24 @@ const router  = express.Router();
 
 router.get('/:id/:userId/quiz_result', (req, res) => {
  const quizId = req.params.id;
- console.log('this is quizID', quizId)
+ console.log('this is quizID:', quizId)
 
  const userId = req.params.userId;
- console.log('this is userID', userId)
+ console.log('this is userID:', userId)
 
  resultQueries.getTotalCorrectAnswers(userId, quizId)
  .then((correctAnswers) => {
-   console.log('This is the number of correct answers',correctAnswers)
+   console.log('This is the number of correct answers:',correctAnswers)
    resultQueries.getTotalQuestions(quizId)
    .then((totalQuestions) => {
-    console.log('This is the number of total questions', totalQuestions)
+    console.log('This is the number of total questions:', totalQuestions)
      let score = correctAnswers.result // becasue the results table already has the number and there is no need to calculate this again.
      console.log(score)
       let total = Number(totalQuestions.total_questions); //total number of questions in a quiz.
       console.log(total)
       let scorePercent = Math.floor((score / total)* 100);
+      console.log('This is the score percentage:', scorePercent)
 
-      //  console.log(quiz_id);
       const templateVars = { score, total, scorePercent }
       res.render('results', templateVars);
     })
