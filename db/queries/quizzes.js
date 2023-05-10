2772d25243c6d642a2af0de6bf11194acb5db920
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-10 10:27:33 -0400

    Implement submitQuestion query function in POST router

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -1,82 +1,87 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const getPublicQuizzes = () => {
-  return db.query(
-    `SELECT * FROM quizzes WHERE private = false`
-  )
-  .then ((res) => {
+  return db.query(`SELECT * FROM quizzes WHERE private = false`).then((res) => {
     return res.rows;
-  })
-}
+  });
+};
 
 const getQuizzesByUserId = (id) => {
-  return db.query((
-    `SELECT * FROM quizzes WHERE user_id IN ($1)`),[id])
-    .then ((res) => {
+  return db
+    .query(`SELECT * FROM quizzes WHERE user_id IN ($1)`, [id])
+    .then((res) => {
       return res.rows;
-    })
-}
+    });
+};
 
 const getQuestionsByQuizId = (id) => {
-  return db.query((
-    `SELECT * FROM questions WHERE quiz_id IN ($1)`),[id])
-    .then ((res) => {
+  return db
+    .query(`SELECT * FROM questions WHERE quiz_id IN ($1)`, [id])
+    .then((res) => {
       return res.rows;
-    })
-}
+    });
+};
 
 // submitQuiz takes user_id, name, private, and private_id
 const submitQuiz = (user_id, name, private, private_id) => {
-
   // SQL query inserts a new row into 'quizzes' table
   //RETURNING id returned so it can be inserted as quiz_id when inserting questions
-  const queryText = 'INSERT INTO quizzes(user_id, name, private, private_id) VALUES($1, $2, $3, $4) RETURNING id';
+  const queryText =
+    'INSERT INTO quizzes(user_id, name, private, private_id) VALUES($1, $2, $3, $4) RETURNING id';
 
   //Values to be inserted into db
   const values = [user_id, name, private, private_id];
 
   // return db.query returns a promise that resolves to the result of query
-  return db.query(queryText, values)
-    //query result passed to callback function
-    .then((res) => {
-      //res.rows returns array of object.  First element in that array is our inserted object, so storing value of 'id' for that object
-      const quiz_id = res.rows[0].id;
-      //quiz_id returned
-      return quiz_id;
-    })
-    //.catch captures any potential error and logs error for query if truthy
-    .catch((err) => {
-      console.error(`Error executing submitQuiz query:`, err);
-    });
+  return (
+    db
+      .query(queryText, values)
+      //query result passed to callback function
+      .then((res) => {
+        //res.rows returns array of object.  First element in that array is our inserted object, so storing value of 'id' for that object
+        const quiz_id = res.rows[0].id;
+        //quiz_id returned
+        return quiz_id;
+      })
+      //.catch captures any potential error and logs error for query if truthy
+      .catch((err) => {
+        console.error(`Error executing submitQuiz query:`, err);
+      })
+  );
 };
-
 
 // submitQuestion takes quiz_id from submitQuiz implementation, and question_number, question from form submit
 const submitQuestion = (quiz_id, question_number, question) => {
-
   //RETURNING id returned so it can be inserted as quiz_id when inserting answers
-  const queryText = 'INSERT INTO questions(quiz_id, question_number, question) VALUES($1, $2, $3) RETURNING id';
+  const queryText =
+    'INSERT INTO questions(quiz_id, question_number, question) VALUES($1, $2, $3) RETURNING id';
 
   //Values to be inserted in db
   const values = [quiz_id, question_number, question];
 
   // return db.query returns a promise that resolves to the result of query
-  return db.query(queryText, values)
-    //query results passed to callback function
-    .then((res) => {
-      //res.rows returns array of object.  First element in that array is our inserted object, so storing value of 'id' for that object
-      const question_id = res.rows[0].id;
+  return (
+    db
+      .query(queryText, values)
+      //query results passed to callback function
+      .then((res) => {
+        //res.rows returns array of object.  First element in that array is our inserted object, so storing value of 'id' for that object
+        const question_id = res.rows[0].id;
 
-      //question_id returned
-      return question_id;
-    })
-    .catch((err) => {
-      //.catch captures any potential error and logs error for query if truthy
-      console.error(`Error executing submitQuestion query:`, err);
-    });
+        //question_id returned
+        return question_id;
+      })
+      .catch((err) => {
+        //.catch captures any potential error and logs error for query if truthy
+        console.error(`Error executing submitQuestion query:`, err);
+      })
+  );
 };
 
-
-
-
-module.exports = {getPublicQuizzes, getQuizzesByUserId, getQuestionsByQuizId, submitQuiz, submitQuestion};
+module.exports = {
+  getPublicQuizzes,
+  getQuizzesByUserId,
+  getQuestionsByQuizId,
+  submitQuiz,
+  submitQuestion,
+};
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,63 +1,93 @@
-const { submitQuiz } = require('../db/queries/quizzes');
-const questionQueries = require('../db/queries/questions')
-const answerQueries = require('../db/queries/answers')
+const { submitQuiz } = require("../db/queries/quizzes");
+const questionQueries = require("../db/queries/questions");
+const answerQueries = require("../db/queries/answers");
 const { addPrivateId, addUserId } = require("../helpers/new_quiz_helpers");
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/new', (req, res) => {
-  res.render('new-quiz');
+router.get("/new", (req, res) => {
+  res.render("new-quiz");
 });
-
-router.post('/new', (req, res) => {
+////////////////////////////////////////////
+//POST to '/new' endpoint where quiz will be submitted
+router.post("/new", (req, res) => {
+  //Adds private_id property to the submitted quiz object
   addPrivateId(req.body);
+
+  //Adds user_id property to the submitted quiz object
   addUserId(req);
+
+  //req.body assigned to quizOnj var
   const quizObj = req.body;
+
+  //TEST log quizObj
   console.log("QUIZ OBJECT:", quizObj);
 
-  submitQuiz(quizObj.user_id, quizObj.quiz_name, quizObj.private, quizObj.private_id)
-    .then((quiz_id) => {
-      console.log("QUIZ ID", quiz_id);
-    })
-    .catch((err) => {
-      console.error(`Error: ${err}`);
-    });
+  //submitQuiz query promise function inserts row into quizzes table in db, and returns quiz_id as resolve
+  submitQuiz(
+    quizObj.user_id,
+    quizObj.quiz_name,
+    quizObj.private,
+    quizObj.private_id
+  ).then((quiz_id) => {
+    //TEST log quiz_id
+    console.log("QUIZ ID", quiz_id);
 
-  res.render('new-quiz');
+    //loop 5 times, once for each question
+    for (let i = 1; i <= 5; i++) {
+      //Use template literals to access key related to each question in quizObj
+      let questionKey = `question${i}`;
+      //questionText is the value of the questionKey property in quizObj
+      let questionText = quizObj[questionKey];
+
+      // submitQuestion query promise function inserts row for each question into questions table.  Will loop 5 times (once for each question)
+      submitQuestion(quiz_id, i, questionText)
+        .then((question_id) => {
+          // TEST log question_id for each question
+          console.log(`QUESTION ID for ${questionKey}`, question_id);
+        })
+
+        //Handle errors from promise chain
+        .catch((err) => {
+          console.error("Error: ", err);
+        });
+    }
+  });
+
+  res.render("new-quiz");
 });
 
-router.get('/submit_quiz', (req, res) => {
-  res.render('quiz_submission');
+///////////////////////////////////////////
+router.get("/submit_quiz", (req, res) => {
+  res.render("quiz_submission");
 });
 
-router.post('/submit_quiz', (req, res) => {
+router.post("/submit_quiz", (req, res) => {
   // should render results page.
-  res.render('....');
+  res.render("....");
 });
 
-
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const quizId = req.params.id;
 
-  questionQueries.getQuestionsByQuizId(quizId)
-  .then((questions) => {
-    answerQueries.getAnswersByQuizId(quizId)
-    .then((answers) => {
-      const templateVars = {questions, answers}
-      // console.log("templateVars", templateVars)
-      res.render('questions', templateVars);
+  questionQueries
+    .getQuestionsByQuizId(quizId)
+    .then((questions) => {
+      answerQueries.getAnswersByQuizId(quizId).then((answers) => {
+        const templateVars = { questions, answers };
+        // console.log("templateVars", templateVars)
+        res.render("questions", templateVars);
+      });
     })
-  })
-  .catch((err) => {
-    res.send(err)
-  })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
-router.post('/:id', (req, res) => {
-  const {answer} = req.body;
+router.post("/:id", (req, res) => {
+  const { answer } = req.body;
   console.log(answer);
-})
-
+});
 
 module.exports = router;
