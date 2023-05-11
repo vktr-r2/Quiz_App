adccd42dd43c30bdcaf886f9df9cff8d18bf9933
Chansoo Han <devhan1207@gmail.com>
2023-05-10 22:58:18 -0400

    fixed to get questions ordered

--- db/queries/questions.js
+++ db/queries/questions.js
@@ -4,7 +4,8 @@ const getQuestionsByQuizId = (id) => {
   return db.query((
     `SELECT quizzes.name, questions.* FROM quizzes
       JOIN questions ON questions.quiz_id = quizzes.id
-      WHERE quizzes.id = $1`), [id])
+      WHERE quizzes.id = $1
+      ORDER BY questions.question_number`), [id])
     .then ((res) => {
       // console.log(res.rows);
       return res.rows;
