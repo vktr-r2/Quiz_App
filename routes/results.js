1c7645884b9c8ae169894e4467cd37e2ebba09f5
Chansoo Han <devhan1207@gmail.com>
2023-05-11 19:40:58 -0400

    clean codes and add readme file

--- routes/results.js
+++ routes/results.js
@@ -5,26 +5,19 @@ const router  = express.Router();
 
 router.get('/:id/:userId/quiz_result', (req, res) => {
  const quizId = req.params.id;
- console.log('this is quizID:', quizId)
 
  const userId = req.params.userId;
- console.log('this is userID:', userId)
 
  resultQueries.getTotalCorrectAnswers(userId, quizId)
  .then((correctAnswers) => {
-   console.log('This is the number of correct answers:',correctAnswers)
    resultQueries.getTotalQuestions(quizId)
    .then((totalQuestions) => {
-    console.log('This is the number of total questions:', totalQuestions)
 
      let score = correctAnswers.result // because the results table already has the number and there is no need to calculate this again.
-     console.log(score)
 
       let total = Number(totalQuestions.total_questions); //total number of questions in a quiz. i an changing the datatype to Number as it returns the count as a string.
-      console.log(total)
 
       let scorePercent = Math.floor((score / total)* 100);
-      console.log('This is the score percentage:', scorePercent)
 
       let quizName = totalQuestions.name
 
